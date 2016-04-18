23c82db239047d12ab5ec6c940aab0b4e43f8f67
mesa312 <junior05_312@hotmail.com>
2016-04-18 18:05:09 -0400

    3rd commit - tailored forms and connected database

--- AVB Final Project/AllRestaurantsForm.Designer.vb
+++ AVB Final Project/AllRestaurantsForm.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class AllRecipesForm
+Partial Class AllRestaurantsForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,27 +22,19 @@ Partial Class AllRecipesForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.btnViewRecipe = New System.Windows.Forms.Button()
         Me.btnModifyList = New System.Windows.Forms.Button()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.CloseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnClose = New System.Windows.Forms.Button()
         Me.MenuStrip1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'btnViewRecipe
-        '
-        Me.btnViewRecipe.Location = New System.Drawing.Point(68, 295)
-        Me.btnViewRecipe.Name = "btnViewRecipe"
-        Me.btnViewRecipe.Size = New System.Drawing.Size(170, 23)
-        Me.btnViewRecipe.TabIndex = 0
-        Me.btnViewRecipe.Text = "VIew Recipe"
-        Me.btnViewRecipe.UseVisualStyleBackColor = True
         '
         'btnModifyList
         '
-        Me.btnModifyList.Location = New System.Drawing.Point(596, 37)
+        Me.btnModifyList.Location = New System.Drawing.Point(640, 303)
         Me.btnModifyList.Name = "btnModifyList"
         Me.btnModifyList.Size = New System.Drawing.Size(75, 23)
         Me.btnModifyList.TabIndex = 1
@@ -54,7 +46,7 @@ Partial Class AllRecipesForm
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(709, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(927, 24)
         Me.MenuStrip1.TabIndex = 3
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -71,37 +63,46 @@ Partial Class AllRecipesForm
         Me.CloseToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
         Me.CloseToolStripMenuItem.Text = "close"
         '
-        'ListBox1
+        'DataGridView1
         '
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.Location = New System.Drawing.Point(58, 53)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(509, 225)
-        Me.ListBox1.TabIndex = 4
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(22, 37)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(810, 252)
+        Me.DataGridView1.TabIndex = 4
         '
-        'AllRecipesForm
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(757, 303)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 5
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'AllRestaurantsForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(709, 373)
-        Me.Controls.Add(Me.ListBox1)
+        Me.ClientSize = New System.Drawing.Size(927, 351)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.btnModifyList)
-        Me.Controls.Add(Me.btnViewRecipe)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
-        Me.Name = "AllRecipesForm"
-        Me.Text = "Form2"
+        Me.Name = "AllRestaurantsForm"
+        Me.Text = "All Restaurants"
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents btnViewRecipe As Button
     Friend WithEvents btnModifyList As Button
     Friend WithEvents MenuStrip1 As MenuStrip
     Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents CloseToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnClose As Button
 End Class
---
+++ AVB Final Project/AllRestaurantsForm.vb
@@ -0,0 +1,13 @@
+﻿Public Class AllRestaurantsForm
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+
+    Private Sub CloseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CloseToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+End Class
